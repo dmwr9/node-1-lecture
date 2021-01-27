e23ea6384a758625340fb70fc837fe3a5765cf0a
Adam Kent <adamkentdev@gmail.com>
2021-01-27 12:29:28 -0700

    finished

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
-
+// express docs: http://expressjs.com/en/api.html
 const app = express();
 
 // ! Top Level Middleware
 app.use(express.json());
 
 const port = 4040;
+
+const users = [{id: 1, name: 'Cole'}, {id:2, name: 'Ben'}, {id:3, name: 'Fred Flinstone'}]
+
+// * General request for all data
+app.get('/api/users',(req, res) => {
+    res.status(200).send(users)
+});
+// * Using params to request one specific piece of data
+app.get('/api/users/:id', (req, res) => {
+    console.log(req.params)
+    let banana = users.find(elem => elem.id === +req.params.id)
+    if(!banana){
+        res.status(404).send("Invalid user id")
+    } else {
+        res.status(200).send(banana)
+    }
+})
+// * Using query to request a specific piece of data
+// ? 'http://localhost:4040/api/user?name=Cole'
+app.get('/api/user', (req, res) => {
+    console.log(req.query)
+    let papaya = users.find(elem => elem.name === req.query.name)
+    res.status(200).send(papaya)
+})
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
